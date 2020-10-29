caa3e9a545aa19538fc7950cf3d754b0b65752c1
pajid9763 <pajid9763@wrdsb.ca>
2020-10-29 15:34:06 +0000

    finished lesson 1

--- Main.java
+++ Main.java
@@ -5,7 +5,21 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+  JLabel firstSideLabel;
+  JLabel secondSideLabel;
+  JLabel thirdSideLabel;
+
+  JTextField firstSideInput;
+  JTextField secondSideInput;
+  JTextField thirdSideInput;
+
+  JButton validateButton;
+  JButton resetButton;
+
+  JTextArea outputText;
+  JTextArea instructionText;
+
 
 
   // Method to assemble our GUI
@@ -18,8 +32,78 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
+
+    // initialize the main JPanel
+    mainPanel = new JPanel();
+    // turn on the manual layouts 
+    mainPanel.setLayout(null);
+    // add the panel to JFrame to see it
+    frame.add(mainPanel);
+
+
+    // create the Labels that let the user know what to enter
+    firstSideLabel = new JLabel("Enter the first side:");
+    secondSideLabel = new JLabel("Enter the second side:");
+    thirdSideLabel = new JLabel("Enter the third side:");
+
+    // set the location and size of the Labels
+    firstSideLabel.setBounds(10, 10, 150, 20);
+    secondSideLabel.setBounds(10, 40, 170, 20);
+    thirdSideLabel.setBounds(10, 70, 150, 20);
     
+    // add the labels to the mainPanel so we can see it
+    mainPanel.add(firstSideLabel);
+    mainPanel.add(secondSideLabel);
+    mainPanel.add(thirdSideLabel);
+
+    // initialize the JTextFields
+    firstSideInput = new JTextField();
+    secondSideInput = new JTextField();
+    thirdSideInput = new JTextField();
+
+    // set the location and size for the input fields
+    firstSideInput.setBounds(200, 10, 100, 20);
+    secondSideInput.setBounds(200, 40, 100, 20);
+    thirdSideInput.setBounds(200, 70, 100, 20);
+
+    // add the input to the main panel
+    mainPanel.add(firstSideInput);
+    mainPanel.add(secondSideInput);
+    mainPanel.add(thirdSideInput);
+
+
+    // initialize the JButtons
+    validateButton = new JButton();
+    resetButton = new JButton();
+
+    // set the location and size of the buttons
+    validateButton.setBounds(320, 10, 100, 35);
+    resetButton.setBounds(320, 55, 100, 35);
+
+    // add the buttons to the main panel
+    mainPanel.add(validateButton);
+    mainPanel.add(resetButton);
+
+
+    // initialize the JButtons
+    outputText = new JTextArea();
+    instructionText = new JTextArea();
+
+    // set the location and size of the buttons 
+    outputText.setBounds(10, 100, 780, 245);
+    instructionText.setBounds(10, 355, 780, 245);
+
+    // disable the textAreas so the user can't type in them
+    outputText.setEnabled(false);
+    instructionText.setEnabled(false);
+
+    // put some text inside instructionText
+    instructionText.setText("This is a simple Triangle detector.\nEnter an integer in each of the text fields above.\nPress the button \"Validate\" to find out whether you have made a valid triangle or not.\nPress the \"Clear\" button to clear the previous text.");
+
+    // add the buttons to the main panel
+    mainPanel.add(outputText);
+    mainPanel.add(instructionText);
+
 
   }
 
